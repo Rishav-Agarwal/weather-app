f7b95e3d0f8594a9a16c15a86ac12f53cfae8765
Ambuj Kumar <itsambuja@gmail.com>
2019-03-11 20:19:52 +0530

    Updates

---
+++ precache-manifest.60804a458a724c802fd74790b473ed76.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/weather-app/static/media/bg.b6796c73.jpg"
+  },
+  {
+    "revision": "90e6a6c358468ae00209",
+    "url": "/weather-app/static/js/runtime~main.4606d7f5.js"
+  },
+  {
+    "revision": "090ff706582887050b15",
+    "url": "/weather-app/static/js/main.6b8d88e2.chunk.js"
+  },
+  {
+    "revision": "f4ae56556fe9e38a0618",
+    "url": "/weather-app/static/js/2.6607d977.chunk.js"
+  },
+  {
+    "revision": "090ff706582887050b15",
+    "url": "/weather-app/static/css/main.407da199.chunk.css"
+  },
+  {
+    "revision": "f4ae56556fe9e38a0618",
+    "url": "/weather-app/static/css/2.fd679b0e.chunk.css"
+  },
+  {
+    "revision": "ec136d644622846ec76e7e6e160a33f7",
+    "url": "/weather-app/index.html"
+  }
+];
