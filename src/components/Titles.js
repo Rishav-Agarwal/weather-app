84a060396fbae19269e0429d0c1b29b8acc793e2
Ambuj Kumar <itsambuja@gmail.com>
2019-03-11 19:22:48 +0530

    bootstrap added

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
 const Titles = () => (
-    <h1>Weather Finder App</h1>
+    <div>
+        <h1 className="title-container__title">Weather Finder App</h1>
+        <p className="title-container__subtitle">Find out temperature, conditions and more...</p>
+    </div>
 );
 export default Titles;
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,12 +2,30 @@ import React from 'react';
 
 // stateless widget
 const Weather = (props) => (
-    <div>
-        { props.city && props.temperature && <p>Location : { props.city }, { props.country } </p>}
-        { props.temperature && <p>Temperature : { props.temperature }&#8451;</p>}
-        { props.humidity && <p>Humidity : { props.humidity } </p>}
-        { props.description && <p>Condition : { props.description } </p>}
-        { props.error && <p>{props.error}</p>}                
+    <div className="weather__info">
+        { 
+            props.city && props.temperature && <p className="weather__key">Location : 
+            <span className="weather__value">{ props.city }, { props.country }</span> 
+            </p>
+        }
+        { 
+            props.temperature && <p className="weather__key">Temperature : 
+            <span className="weather__value">{ props.temperature }&#8451;</span>
+            </p>
+        }
+        { 
+            props.humidity && <p className="weather__key">Humidity : 
+            <span className="weather__value">{ props.humidity } </span></p>
+        }
+        { 
+            props.description && <p className="weather__key">Condition : 
+            <span className="weather__value">{ props.description } </span></p>
+        }
+        { 
+            props.error && <p className="weather__key">
+            <span>{props.error}</span>
+            </p>
+        }
     </div>
 );
 
